9bd97ca0e98437c6b0789ba7139c7848c275aa2c
Zaeem629 <zaeemuzzafar@gmail.com>
2022-06-08 05:37:42 +0500

    changes made

--- src/components/Summer.js
+++ src/components/Summer.js
@@ -8,6 +8,9 @@ function Summer() {
         <img src={img} width={300} height={300} />
     </div>
     <div>
+        <span><i className="massive loading sun icon"></i></span>
+    </div>
+    <div>
         <h1>Too hort lets hit the beach</h1>
     </div>
     </>
